a7bc899909ae3a93b50ef7ca295bd413fe0f8841
André Lima <me@limwa.pt>
2024-04-18 17:05:03 +0100

    feat: add output tabs

--- src/app/compile.tsx
+++ src/app/compile.tsx
@@ -2,12 +2,15 @@
 
 import { useFormState } from "react-dom";
 import { compileJmm } from "./actions";
+import { useMemo, useState } from "react";
 
 export function Compile() {
   const [[stdout, stderr], dispatch, isPending] = useFormState(compileJmm, [
-    "--stdout--",
-    "--stderr--",
+    "",
+    "",
   ]);
+
+  const hasErrors = stderr !== "";
 
   return (
     <div className="mt-8 grow flex justify-between gap-4">
@@ -42,18 +45,97 @@ export function Compile() {
         </button>
       </form>
       <div className="size-full flex flex-col">
-        <h2 className="text-lg font-light">The output and error goes here</h2>
+        {hasErrors ? (
+            <>
+        <p className="text-lg font-light">Your code has errors...</p>
         <textarea
-          readOnly
-          className="mt-2 text-black font-mono p-4 grow rounded"
-          value={stdout}
-        ></textarea>
-        <textarea
-          readOnly
-          className="mt-2 text-black font-mono p-4 grow rounded"
-          value={stderr}
-        ></textarea>
+            readOnly
+            className="mt-2 text-black font-mono p-4 grow rounded"
+            value={stderr}
+          ></textarea>
+            </>
+          
+        ) : (
+            <>
+            <p className="text-lg font-light">Your code compiles properly...</p>
+            <CompileOutput stdout={stdout} />
+            </>
+        )}
       </div>
+    </div>
+  );
+}
+
+function slices(content: string, markers: string[]) {
+    const slices = [] as string[];
+
+    let start = content.indexOf(markers[0]);
+    if (start === -1) return null;
+
+    start += markers[0].length;
+
+    for (let i = 1; i < markers.length; i++) {
+        const end = content.indexOf(markers[i]);
+        if (end === -1) return null;
+
+        const slice = content.slice(start, end).trim();
+        slices.push(slice);
+        start = end + markers[i].length;
+    }
+
+    const lastSlice = content.slice(start).trim();
+    slices.push(lastSlice);
+
+    return slices;
+}
+
+export function CompileOutput({ stdout }: { stdout: string }) {
+  const obj = useMemo(() => {
+    const results = slices(stdout, [
+        "Symbol Table:",
+        "AST:",
+        "OPTIMIZED AST:",
+        "OLLIR RESULT:",
+        "OPTIMIZED OLLIR RESULT:",
+        "JASMIN CODE:",
+        ]);
+
+    if (results === null) {
+        return {
+            "Symbol Table": "",
+            AST: "",
+            "Optimized AST": "",
+            OLLIR: "",
+            "Optimized OLLIR": "",
+            Jasmin: "",
+        };
+    }
+
+    const [symbolTable, ast, optimizedAst, ollir, optimizedOllir, jasmin] = results;
+
+    return {
+      "Symbol Table": symbolTable,
+      AST: ast,
+      "Optimized AST": optimizedAst,
+      OLLIR: ollir,
+      "Optimized OLLIR": optimizedOllir,
+      Jasmin: jasmin,
+    };
+  }, [stdout]);
+
+  const [selectedTab, setSelectedTab] = useState<keyof typeof obj>("Symbol Table");
+
+  return (
+    <div className="size-full flex flex-col">
+      <div className="flex gap-1">
+        {Object.keys(obj).map((key) => (
+          <button key={key} className="px-4 py-2 bg-slate-500 rounded hover:bg-slate-600 transition-colors disabled:bg-purple-700" disabled={selectedTab === key} onClick={() => setSelectedTab(key as keyof typeof obj)}>
+            {key}
+          </button>
+        ))}
+      </div>
+      <textarea value={obj[selectedTab]} readOnly 
+            className="mt-2 text-black font-mono p-4 grow rounded"></textarea>
     </div>
   );
 }
