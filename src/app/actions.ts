7e8f7e9769e04c9a704ac053f38beec68286c22d
André Lima <me@limwa.pt>
2024-05-03 02:04:19 +0100

    chore: add error logging

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -38,32 +38,40 @@ export async function compileJmm(fd: FormData): Promise<ProtocolSection[]> {
     .stderr("piped")
     .noThrow();
 
-  try {
-  await fs.rm(dir, { recursive: true, force: true });
-
-  const matches = process.stdout.matchAll(protocolRegex);
-
-  const sections: ProtocolSection[] = [];
-  for (const match of matches) {
-    const { name, uuid, status, content } = match.groups!;
-    sections.push({
-      name,
-      uuid,
-      status: status as ProtocolSection["status"], // Ensured by the regex
-      content: content.trim(),
+  if (process.stderr) {
+    console.error({
+      type: "Compilation Stderr",
+      stderr: process.stderr,
     });
   }
 
-  return sections;
-} catch (e) {
-  console.error(e);
-  return [
-    {
-      uuid: "internal-error",
-      name: "Internal Error",
-      content: "An unknown error occurred, please try again or contact an administrator.",
-      status: "bad",
+  try {
+    await fs.rm(dir, { recursive: true, force: true });
+
+    const matches = process.stdout.matchAll(protocolRegex);
+
+    const sections: ProtocolSection[] = [];
+    for (const match of matches) {
+      const { name, uuid, status, content } = match.groups!;
+      sections.push({
+        name,
+        uuid,
+        status: status as ProtocolSection["status"], // Ensured by the regex
+        content: content.trim(),
+      });
     }
-  ]
-}
+
+    return sections;
+  } catch (e) {
+    console.error(e);
+    return [
+      {
+        uuid: "internal-error",
+        name: "Internal Error",
+        content:
+          "An unknown error occurred, please try again or contact an administrator.",
+        status: "bad",
+      },
+    ];
+  }
 }
