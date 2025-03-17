6f0ba431856ae1d900ae01e67b02755653bda604
André Lima <me@limwa.pt>
2025-03-17 11:07:00 +0000

    fix: only show last updated on larger devices

--- src/lib/ui/header.tsx
+++ src/lib/ui/header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { cn } from "./utils/cn";
 import { SiGithub } from "@icons-pack/react-simple-icons";
 import { useMemo } from "react";
-import { formatDistance, formatDate } from "date-fns";
+import { formatDistance } from "date-fns";
 import { Tooltip, TooltipContent, TooltipTrigger } from "./tooltip";
 
 function LastUpdated({ lastUpdated }: { lastUpdated: Date }) {
@@ -36,7 +36,11 @@ export function Header({ lastUpdated, className }: { lastUpdated: Date | null, c
       <div className="flex items-center justify-between selection:bg-neutral-400/30">
         <h1 className="font-mono text-lg tracking-wider">jmm compiler</h1>
         <div className="flex gap-10">
-          {lastUpdated && <LastUpdated lastUpdated={lastUpdated} />}
+          {lastUpdated && (
+            <div className="hidden sm:block">
+              {lastUpdated && <LastUpdated lastUpdated={lastUpdated} />}
+            </div>
+          )}
           <Link
             className="ml-auto rounded-full text-black outline-2 outline-offset-1 outline-teal-500 transition-colors hover:text-neutral-700 focus-visible:outline dark:text-white dark:outline-teal-300 dark:hover:text-neutral-300"
             href="https://github.com/limwa/jmm.limwa.pt"
